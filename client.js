fe365edaa96fbf567de017096415205614325474
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-23 13:40:37 +0000

    setting up user input via stdin

--- client.js
+++ client.js
@@ -1,14 +1,30 @@
 const net = require('net');
+const stdin = process.stdin;
+const stdout = process.stdout;
+const exit = process.exit;
 
-/**
- * Establishes connection with the game server
- */
+const setupInput = function() {
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+}();
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    stdout.write('Thanks for using me, ciao!\n');
+    exit();
+  }
+};
+
 const connect = function() {
   const conn = net.createConnection({ 
     host: '10.0.2.15',
     port: 50541
   });
-  // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
