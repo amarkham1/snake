eedfe8f087e4352efe60788ee2531a9a40d14d4e
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-23 13:48:55 +0000

    added wasd functionality

--- input.js
+++ input.js
@@ -1,13 +1,15 @@
 const stdin = process.stdin;
 const stdout = process.stdout;
 const exit = process.exit;
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (key) => {
-    handleUserInput(key);
+    handleUserInput(key, connection);
   });
   return stdin;
 };
@@ -16,6 +18,14 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     stdout.write('Thanks for using me, ciao!\n');
     exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
