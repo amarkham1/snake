6b2ea7220e35cee340109680043a30e7c04b9f8e
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-23 13:54:40 +0000

    moved more to constants file

---
+++ constants.js
@@ -0,0 +1,18 @@
+const { isIP } = require("net");
+const IP = '10.0.2.15';
+const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const SPEAK = 'q';
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  SPEAK
+};
--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, SPEAK } = require('./constants');
+
 const stdin = process.stdin;
 const stdout = process.stdout;
 const exit = process.exit;
@@ -18,15 +20,15 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     stdout.write('Thanks for using me, ciao!\n');
     exit();
-  } else if (key === 'w') {
+  } else if (key === MOVE_UP_KEY) {
     connection.write('Move: up');
-  } else if (key === 'a') {
+  } else if (key === MOVE_LEFT_KEY) {
     connection.write('Move: left');
-  } else if (key === 's') {
+  } else if (key === MOVE_DOWN_KEY) {
     connection.write('Move: down');
-  } else if (key === 'd') {
+  } else if (key === MOVE_RIGHT_KEY) {
     connection.write('Move: right');
-  } else if (key === 'q') {
+  } else if (key === SPEAK) {
     connection.write('Say: QQ Baby');
   }
 };
